6580a2b4a7f95e2ae5e0ec7b5ff03149004e15ac
rohit <rohit.kumar@glotelligence.in>
2022-04-23 23:35:42 +0530

    blocking code example, keep busy the event loop and stop taking other request

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
+function doWork(duration){
+    const start = Date.now();
+    while (Date.now() - start < duration) {
+
+    }
+}
+
 app.get('/', (req, res)=>{
-    console.log('Hi');
+    doWork(5000);
+    res.send("Hi there")
 });
 app.listen(3000);
