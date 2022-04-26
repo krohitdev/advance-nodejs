fe8d276b942d9df65ae3affd8e9c5c1ce4e45a1b
rohit <rohit.kumar@glotelligence.in>
2022-04-26 22:55:52 +0530

    cluster mode example

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration){
-    const start = Date.now();
-    while (Date.now() - start < duration) {
+// is the file being executed in Master mode
+if (cluster.isMaster) {
+    // Cause index.js to be executed again but
+    // in child mode
+    cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
+} else {
+    // I am a child I am going to act like server 
+    // and do nothing else
+    const express = require('express');
+    const app = express();
 
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) {
+
+        }
     }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send("Hi there")
+    });
+
+    app.get('/fast', (req, res)=>{
+        res.send("FAST ")
+    })
+    app.listen(3000);
 }
 
-app.get('/', (req, res)=>{
-    doWork(5000);
-    res.send("Hi there")
-});
-app.listen(3000);
+
